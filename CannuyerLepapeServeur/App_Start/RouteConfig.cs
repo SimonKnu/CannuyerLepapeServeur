548e335e35ad9ab0553e23a6ae5a36adc2352806
SimonKnu <anderlecht.11@hotmail.com>
2017-11-29 05:07:40 +0100

    f

--- CannuyerLepapeServeur/App_Start/RouteConfig.cs
+++ CannuyerLepapeServeur/App_Start/RouteConfig.cs
@@ -11,18 +11,18 @@ namespace CannuyerLepapeServeur
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("spa-fallback", "{*anything}", new { controller = "Home", action = "Index" });
-            /*
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           routes.MapRoute("spa-fallback", "{*anything}", new { controller = "Home", action = "Index" });
+            
+            /*routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-            */
+            );*/
+            
         }
     }
 }
